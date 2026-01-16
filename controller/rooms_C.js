5e2eca3e0acea9480009b08ef5dd8a367adc0dda
Eli lang <elilang92@gmail.com>
2026-01-16 11:10:25 +0200

    started the rooms Crud

--- controller/rooms_C.js
+++ controller/rooms_C.js
@@ -12,3 +12,46 @@ async function getAllRooms(req,res){
         res.status(500).json({message:'server error'});
     }
 }
+
+async function getRoom(req,res){
+    try{
+        let room = await getRoomById(req.params.id);
+        if(!room){
+            return res.status(400).json({message:'room not found'});
+        }
+        res.status(200).json(room);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message:'server error'});
+    }
+}
+
+async function deleteRoom(req,res){
+    try{
+        let affectedRows = await deleteRoom(req.params.id);
+        if(!affectedRows){
+            return res.status(400).json({message:'room not found'});
+        }
+        res.status(200).json({message:'room deleted'});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message:'server error'});
+    }
+}
+
+async function updateRoom(req,res){
+    try{
+        let affectedRows = await updateRoom(req.params.id, req.body);
+        if(!affectedRows){
+            return res.status(400).json({message:'room not found'});
+        }
+        res.status(200).json({message:'room updated'});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message:'server error'});
+    }
+}
+
+async function addRoom(req,res){
+
+module.exports = {getAllRooms, getRoom, deleteRoom, updateRoom, addRoom};
